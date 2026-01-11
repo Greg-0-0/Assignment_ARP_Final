8c46be616a8af76f06c5334f316f37512b589212
Gregorio Daneri <greg.daneri@gmail.com>
2026-01-12 00:32:58 +0100

    Deleted files

--- execute.sh
+++
@@ -1,7 +0,0 @@
-gcc master.c functions.c -o master -lncurses -lm
-gcc -Wall -Wextra blackboard.c functions.c -lncurses -lm -o blackboard 
-gcc drone.c functions.c -o drone -lncurses -lm
-gcc input_manager.c -lncurses -o input_manager
-gcc obstacles.c functions.c -o obstacles -lncurses -lm
-gcc targets.c functions.c -o targets -lncurses -lm
-./master
